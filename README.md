04a62d1104dc6a8e93cc3e16707f518fdb714ccf
mrredamber <kadenlnelson@gmail.com>
2016-12-03 16:18:31 -0700

    added some changes and a working docker-compose file

--- README.md
+++ README.md
@@ -4,10 +4,7 @@ v0.1 (BETA)
 ![](https://pbs.twimg.com/profile_images/3590976478/37d04beba0abebc0212070df35c511e9.jpeg)
 ###  Docker usage
 Building from source
-```bash
-$ docker build -t nekobot .
-```
 Running with docker
 ```bash
-$ docker run --env-file nekobot.rc -it --rm --name nekobot nekobot
+$ docker-compose up --build
 ```
--- bot/plugins/nekobot.py
+++ bot/plugins/nekobot.py
@@ -17,8 +17,30 @@ class Nekobot(Plugin):
     plugin_name = 'Nekobot'
     channel_name = 'nekobot'
 
+    async def get_commands(self):
+        commands = [
+            {
+                'name': '!whoareyou',
+                'description': 'Ask Nekobot bot who she is'
+            },
+        ]
+        return commands
+
     @channel_specific
     async def on_message(self, message, **kwargs):
+        # Check for commands
+        if message.content.startswith('!whoareyou'):
+            await self.nekobot.send_message(
+                message.channel,
+                'I\'m a chat bot and I was born on December 1st 2016. \n'
+                'My owner\'s name is Kaden but he likes to be called MrRedAmber.\n'
+                'He isn\'t shy for constructive criticism and will is always\n'
+                'open to take suggestions.\n\nMessage from MrRedAmber: \nI\'m'
+                ' sorry if nekobot isn\'t working to well. She\'s still just a kitten \n'
+                'and constantly learning about the brave new world around her!\n'
+                'Check us out on github! https://github.com/MrRedAmber/nekobot.'
+            )
+
         # Check if a greeting is in the message
         for greeting in greetings:
             if greeting in message.content:
