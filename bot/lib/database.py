665cbb6bb66f5cf6994baaa5b4429d18e9950c98
mrredamber <kadenlnelson@gmail.com>
2016-12-07 08:08:09 -0700

    this file is going to go ancient because Im too focuzed on other code

--- bot/lib/database.py
+++ bot/lib/database.py
@@ -16,15 +16,3 @@ class Database:
         # Get collection from the database handle
         self.collection = self.db['blog_post']
 
-        log.info('Databases: {0}\n Collections: {1}\n Files: {2}'.format(
-            self.client.database_names(), self.db.collection_names(), self.collection.count()
-        ))
-        # TODO: REMOVE
-        try:
-            blogCol = self.collection.find()
-            log.info('\n All data from the blog_data database \n')
-            for blog in blogCol:
-                log.info(blog)
-
-        except Exception as e:
-            print(str(e))
