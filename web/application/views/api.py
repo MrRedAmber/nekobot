4fea6ed19b9812a2e5e22e74e0af92ad617b46dc
MrRedAmber <kadenlnelson@gmail.com>
2018-01-26 13:40:22 -0700

    dusting off

--- web/application/views/api.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-
-
-api = Blueprint('api', __name__, url_prefix='/api')
-
-
-@api.route('/')
-def api_get_index():
-    return jsonify(message='I\'ve got a bad feeling about this...'), 200
